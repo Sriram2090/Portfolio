0b5a72fcac1184b5d0cb724ceabe22e372a02c15
Sriram2090 <sriramb855@gmail.com>
2024-07-25 13:53:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+//var,let,const--> key words
+//syntax ---> variable_keyword 
+ /*var a=10; //variable assigning or declaring
+console.log(typeof a);
+console.log(a);
+
+a=20;
+console.log(a);// varial=ble reassigning
+
+
+ var a=30;//variable redeclaring
+console.log(a);*/
+
+
